8894a0f2f8aed774f78b84c740e8a7df080bf655
Sokolov Dmitrii <sokolov.d@alcor.co.ru>
2024-06-13 08:28:30 +0300

    Test 3 add to loop for

--- stepik/src/lesson14/Test2.java
+++ stepik/src/lesson14/Test2.java
@@ -2,8 +2,9 @@ package lesson14;
 
 public class Test2 {
     public static void main(String[] args) {
-        for (int i = 5; i < 50; i = i + 5) {
-            System.out.println(i);
+        for (int i = 0, j = 20; i < 50 && j < 30; i = i + 2, j++) {
+            System.out.println("i " + i);
+            System.out.println("j " + j);
         }
     }
 }
