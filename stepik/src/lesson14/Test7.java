c85533618e872a0f8e092ace73fe9a79b05838f1
Sokolov Dmitrii <sokolov.d@alcor.co.ru>
2024-06-13 09:36:35 +0300

    homework lesson 14

--- stepik/src/lesson14/Test7.java
+++ stepik/src/lesson14/Test7.java
@@ -4,13 +4,13 @@ public class Test7 {
     public static void time() {
         OUTER1:
         for (int chas = 0; chas <= 5; chas++) {
-            for (int min = 0; min <= 59; min++) {
+            MIDDLE:for (int min = 0; min <= 59; min++) {
                 if (chas > 1 && min % 10 == 0) {
                     break OUTER1;
                 }
                 for (int sec = 0; sec <= 59; sec++) {
                     if (sec * chas > min) {
-                        continue;
+                        continue MIDDLE;
                     }
                     System.out.println(chas + ":" + min + ":" + sec);
                 }
