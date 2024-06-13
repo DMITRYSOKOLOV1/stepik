56fc075d1ec5eaefa31daf93f1dfa149e2bd9392
Sokolov Dmitrii <sokolov.d@alcor.co.ru>
2024-06-13 08:12:26 +0300

    loop for

--- stepik/src/lesson14/Test2.java
+++ stepik/src/lesson14/Test2.java
@@ -2,7 +2,7 @@ package lesson14;
 
 public class Test2 {
     public static void main(String[] args) {
-        for (int i = 0; i < 100; i = i + 10) {
+        for (int i = 5; i < 50; i = i + 5) {
             System.out.println(i);
         }
     }
