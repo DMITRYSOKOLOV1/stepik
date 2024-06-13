06740e443095c484c315ac44cc0e60fd735981c6
Sokolov Dmitrii <sokolov.d@alcor.co.ru>
2024-06-13 08:54:48 +0300

    break and continue into loop

---
+++ stepik/src/lesson14/Test4.java
@@ -0,0 +1,16 @@
+package lesson14;
+
+public class Test4 {
+    public static void main(String[] args) {
+        for (int i = 2; i <=4; i=i+2){
+            System.out.println(i);
+        }
+
+        int a = 5;
+        if (a < 10){
+            for (int i = 1; i<=10;i++){
+                System.out.println("i - " + i);
+            }
+        }
+    }
+}
