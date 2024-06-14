b650b23c1756882e4ece35d1420b78131f884420
Sokolov Dmitrii <sokolov.d@alcor.co.ru>
2024-06-14 14:03:08 +0300

    lesson16 create String

---
+++ stepik/src/lesson16/Test1.java
@@ -0,0 +1,11 @@
+package lesson16;
+
+public class Test1 {
+    public static void main(String[] args) {
+        /*cоздание объектов String*/
+        String s1 = new String("Hello world");
+        String s2 = new String("Hello world");
+        String s3 = "Hello world";
+        System.out.println("\"Hello world\"");
+    }
+}
