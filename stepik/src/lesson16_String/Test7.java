a5034f54e05c18ed3cabf5e093408e3d7aebfe2e
Sokolov Dmitrii <sokolov.d@alcor.co.ru>
2024-06-17 10:51:03 +0300

    Test7 пример

--- stepik/src/lesson16_String/Test7.java
+++ stepik/src/lesson16_String/Test7.java
@@ -4,7 +4,7 @@ public class Test7 {
     public static void main(String[] args) {
         String str = "Hello World";
         String str2 = "Hello";
-        String str3 = str.concat(str2).trim().replace("Hello","Privet").substring(1,5);
+        String str3 = str.concat(str2).trim().replace("Hello","Privet").substring(6,10);
         System.out.println(str.substring((str.indexOf('W'))));
     }
 }
