65e96c4cccd53f94ed467977ec494442a53318c4
Sokolov Dmitrii <sokolov.d@alcor.co.ru>
2024-06-17 10:20:55 +0300

    lesson16 методы String + примеры конкатенации

---
+++ stepik/src/lesson16/Test4.java
@@ -0,0 +1,12 @@
+package lesson16;
+
+public class Test4 {
+
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 6;
+        String s = "OK";
+        System.out.println("" + a + b + s);
+    }
+}
+
