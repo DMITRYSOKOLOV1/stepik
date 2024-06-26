d688a5766722f1804c02c394367ebb938ec34c69
Sokolov Dmitrii <sokolov.d@alcor.co.ru>
2024-06-26 10:31:18 +0300

    ArrayList methods add

--- stepik/src/lesson20_ArrayList/Test2.java
+++ stepik/src/lesson20_ArrayList/Test2.java
@@ -5,6 +5,12 @@ import java.util.ArrayList;
 public class Test2 {
     public static void main(String[] args) {
         ArrayList <String> list = new ArrayList <>();
-        list.add("A");
+        list.add("Hello");
+        list.add("World");
+        list.add("Java");
+        list.add(0,"Python");
+        for (String s : list) {
+            System.out.println(s);
+        }
     }
 }
