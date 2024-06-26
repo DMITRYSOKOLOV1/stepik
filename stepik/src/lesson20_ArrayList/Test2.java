92535ac1ba61b46f33525c12ba2f1172a2bc908c
Sokolov Dmitrii <sokolov.d@alcor.co.ru>
2024-06-26 10:56:12 +0300

    ArrayList methods get set

--- stepik/src/lesson20_ArrayList/Test2.java
+++ stepik/src/lesson20_ArrayList/Test2.java
@@ -4,13 +4,21 @@ import java.util.ArrayList;
 
 public class Test2 {
     public static void main(String[] args) {
-        ArrayList <String> list = new ArrayList <>();
+        ArrayList<String> list = new ArrayList<>();
         list.add("Hello");
         list.add("World");
         list.add("Java");
-        list.add(0,"Python");
+        list.add(0, "Python");
         for (String s : list) {
             System.out.println(s);
         }
+
+        System.out.println(list.get(0));
+        for (String s : list) {
+            System.out.println(s);
+        }
+
+        list.set(0, "Letoile");
+        System.out.println(list.get(0));
     }
 }
