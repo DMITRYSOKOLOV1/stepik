4e2e426c4c7570a2a43891ece5dc5909f1720b8e
Sokolov Dmitrii <sokolov.d@alcor.co.ru>
2024-06-27 16:10:39 +0300

    ArrayList method ArrayList equals

--- stepik/src/lesson20_ArrayList/Test6.java
+++ stepik/src/lesson20_ArrayList/Test6.java
@@ -14,7 +14,13 @@ public class Test6 {
         list.add(s4);
         list.add(s2);
         System.out.println(list);
-        Collections.sort(list);
-        System.out.println(list);
+        //Collections.sort(list);
+        //System.out.println(list);
+        ArrayList <String> list2 = new ArrayList <>();
+        list2.add(s3);
+        list2.add(s1);
+        list2.add(s4);
+        list2.add(s2);
+        System.out.println(list.equals(list2));//equals перезаписан
     }
 }
