cf149674b073bb70b147df460dbb733a7eb3a79f
Sokolov Dmitrii <sokolov.d@alcor.co.ru>
2024-06-27 16:19:37 +0300

    ArrayList method Iterator

--- stepik/src/lesson20_ArrayList/Test6.java
+++ stepik/src/lesson20_ArrayList/Test6.java
@@ -1,6 +1,7 @@
 package lesson20_ArrayList;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Iterator;
 
 public class Test6 {
     public static void main(String[] args) {
@@ -22,5 +23,23 @@ public class Test6 {
         list2.add(s4);
         list2.add(s2);
         System.out.println(list.equals(list2));//equals перезаписан
+    }
+}
+class Test6_1{
+    public static void main(String[] args) {
+        String s1 = "a";
+        String s2 = "b";
+        String s3 = "c";
+        String s4 = "d";
+        ArrayList<String> list = new ArrayList<>();
+        list.add(s3);
+        list.add(s1);
+        list.add(s4);
+        list.add(s2);
+
+        Iterator <String> it = list.iterator();
+        while(it.hasNext()){
+            System.out.println(it.next());
+        }
     }
 }
