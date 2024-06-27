1541fb2e7838807f1187fd0c5ec1e9a93402a537
Sokolov Dmitrii <sokolov.d@alcor.co.ru>
2024-06-27 16:06:38 +0300

    ArrayList method Collections.sort()

---
+++ stepik/src/lesson20_ArrayList/Test6.java
@@ -0,0 +1,20 @@
+package lesson20_ArrayList;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Test6 {
+    public static void main(String[] args) {
+        String s1 = "A";
+        String s2 = "B";
+        String s3 = "C";
+        String s4 = "D";
+        ArrayList <String> list = new ArrayList <>();
+        list.add(s3);
+        list.add(s1);
+        list.add(s4);
+        list.add(s2);
+        System.out.println(list);
+        Collections.sort(list);
+        System.out.println(list);
+    }
+}
