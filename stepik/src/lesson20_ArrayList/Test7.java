eac274b600d1f73e94f56e45ec0a05a0d311a66c
Sokolov Dmitrii <sokolov.d@alcor.co.ru>
2024-06-28 08:52:48 +0300

    method Arrays.compare

---
+++ stepik/src/lesson20_ArrayList/Test7.java
@@ -0,0 +1,15 @@
+package lesson20_ArrayList;
+
+import java.util.Arrays;
+
+public class Test7 {
+    public static void main(String[] args) {
+        int [] array1 = {1,2,3,4,5};
+        int [] array2 = {1,2,3,5};
+
+        char [] array3 = {'p','r','i','v','e','t'};
+        char [] array4 = {'p','r','i','v','i','v','k','a'};
+
+        System.out.println(Arrays.compare(array4,array3));//когда одинаковы 0, когда 1 < 2 меньше 0
+    }
+}
