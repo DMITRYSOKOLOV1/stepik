e0c3c699be035959baf29fa2b960404cd500a7eb
Sokolov Dmitrii <sokolov.d@alcor.co.ru>
2024-06-28 11:56:35 +0300

    map

---
+++ stepik/src/lesson20_ArrayList/Test10Map.java
@@ -0,0 +1,15 @@
+package lesson20_ArrayList;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Test10Map {
+    public static void main(String[] args) {
+    Map<Integer, String> map = new HashMap<Integer, String>();
+    map.put(1, "Andrey");
+    map.put(2, "James");
+    map.put(3, "John");
+    map.put(4, "Mary");
+        System.out.println(map);
+    }
+}
