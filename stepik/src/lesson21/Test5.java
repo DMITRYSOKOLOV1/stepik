73b184bc02e8c47af97d503364d9d52aed6007b4
Sokolov Dmitrii <sokolov.d@alcor.co.ru>
2024-07-01 16:57:27 +0300

    Primer

---
+++ stepik/src/lesson21/Test5.java
@@ -0,0 +1,13 @@
+package lesson21;
+
+public class Test5 {
+    void abc(int i){
+        System.out.println(i);
+    }
+
+    public static void main(String[] args) {
+        Test5 test5 = new Test5();
+        char c = 'a';
+        test5.abc(c);
+    }
+}
