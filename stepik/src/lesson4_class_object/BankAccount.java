87a9679aa9e0ac0ad29541739bfbc0fd1ce6dd0c
irinai <petinvasa770@gmail.com>
2025-10-17 15:21:45 +0300

    Class + construktor + object

--- stepik/src/lesson4_class_object/BankAccount.java
+++ stepik/src/lesson4_class_object/BankAccount.java
@@ -4,12 +4,22 @@ public class BankAccount {
     int id;
     String name;
     double balance;
+    BankAccount(){}
+    BankAccount(int id, String name, double balance){
+        this.id = id;
+        this.name = name;
+        this.balance = balance;
 
-    public static void main(String[] args) {
-        BankAccount test = new BankAccount();
-        test.id = 1;
-        test.name = "Dima";
-        test.balance = 150;
+
+
+
+    }
+    static void main() {
+        BankAccount test = new BankAccount(1, "Test", 250.50);
+        BankAccount test2 = new BankAccount();
+        test2.id = 2;
+        test2.balance = 100;
+        test2.name = "Test2";
     }
 }
 
