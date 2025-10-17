784bc703f8833100c27da6edb8a9d4c4e730e404
irinai <petinvasa770@gmail.com>
2025-10-17 14:40:30 +0300

    17.10.2025 lesson4.class bank

---
+++ stepik/src/lesson4_class_object/BankAccount.java
@@ -0,0 +1,16 @@
+package lesson4_class_object;
+
+public class BankAccount {
+    int id;
+    String name;
+    double balance;
+
+    public static void main(String[] args) {
+        BankAccount test = new BankAccount();
+        test.id = 1;
+        test.name = "Dima";
+        test.balance = 150;
+    }
+}
+
+
